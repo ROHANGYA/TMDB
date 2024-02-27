6da39df36184d6775bffc3d2584bc9822283ca86
Rohan <rohgyas@hotmail.com>
2024-02-28 01:24:15 +0800

    Add local/remote project base structure

---
+++ lib/data/data_sources/local/secure_storage/secure_storage_service.dart
@@ -0,0 +1,3 @@
+class SecureStorageService {
+  // Here you init it & create helpers such as setvalue/getvalue/clear/getValueOrNull/listenable.
+}
---
+++ lib/data/data_sources/remote/api_client_provider.dart
@@ -0,0 +1,4 @@
+class ApiClientProvider {
+  // this takes the SINGLE injected dio client and "formats" it accordingly.
+  // Not sure if the above is better than just defining two dio clients.
+}
