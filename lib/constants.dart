0255df77a00382a8692d60e78ca41496acce1251
Rohan <rohgyas@hotmail.com>
2024-02-29 19:38:16 +0800

    Add new project colors

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,1 +1,13 @@
-// urls etc
+import 'package:flutter/material.dart';
+import 'package:tmdb/generated/l10n.dart';
+
+abstract class MyColors {
+  static const vermillionRed = Color(0xFFc84843);
+  static const crayolaGold = Color(0xFFe1c19b);
+  static const persianPlum = Color(0xFF6d201c);
+  static const darkTan = Color(0xFF949054);
+  static const charcoal = Color(0xFF2f3d53);
+  static const darkBlue = Color(0xff171f26);
+}
+
+final strings = S.current;
