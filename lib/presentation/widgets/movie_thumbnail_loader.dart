88e1e38c9366a065fb968073e60409569079f68e
Rohan <rohgyas@hotmail.com>
2024-03-01 22:15:48 +0800

    Add ability to load original resolution images

--- lib/presentation/widgets/movie_thumbnail_loader.dart
+++ lib/presentation/widgets/movie_thumbnail_loader.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:tmdb/constants.dart';
 
 class MovieThumbnailLoader extends StatelessWidget {
-  const MovieThumbnailLoader({super.key, required this.imageUrl});
+  const MovieThumbnailLoader(
+      {super.key, required this.imageUrl, this.originalQuality = false});
 
   final String imageUrl;
+  final bool originalQuality;
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +18,7 @@ class MovieThumbnailLoader extends StatelessWidget {
           width: 140,
           child: Image.network(
             fit: BoxFit.fill,
-            "${ApiUrl.imageBaseUrl}$imageUrl",
+            "${originalQuality ? ApiUrl.imageUrlOriginal : ApiUrl.imageUrl500w}$imageUrl",
           ),
         ));
   }
