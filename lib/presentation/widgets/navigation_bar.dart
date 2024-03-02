fd3f61b7ce6d65c6730b48b85a5b35bf86c05d97
Rohan <rohgyas@hotmail.com>
2024-03-02 16:21:50 +0800

    Update navigation bar to have state

--- lib/presentation/widgets/navigation_bar.dart
+++ lib/presentation/widgets/navigation_bar.dart
@@ -1,7 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:tmdb/constants.dart';
 
-class MovieNavigationBar extends StatelessWidget {
+class MovieNavigationBar extends StatefulWidget {
   const MovieNavigationBar({super.key});
+
+  @override
+  State<MovieNavigationBar> createState() => _MovieNavigationBarState();
+}
+
+class _MovieNavigationBarState extends State<MovieNavigationBar> {
+  final navDestinations = [
+    NavigationDestination(
+      icon: const Icon(Icons.home_outlined),
+      label: strings.home,
+    ),
+    NavigationDestination(
+      icon: const Icon(Icons.search_rounded, size: 29),
+      label: strings.search,
+    ),
+    NavigationDestination(
+      icon: const Icon(Icons.settings_rounded),
+      label: strings.settings,
+    )
+  ];
+  int currentPageIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -11,26 +33,12 @@ class MovieNavigationBar extends StatelessWidget {
           borderRadius: BorderRadius.circular(14),
           child: NavigationBar(
             onDestinationSelected: (int index) {
-              // setState(() {
-              //   currentPageIndex = index;
-              // });
+              setState(() {
+                currentPageIndex = index;
+              });
             },
-            selectedIndex: 0,
-            destinations: const <Widget>[
-              NavigationDestination(
-                selectedIcon: Icon(Icons.home),
-                icon: Icon(Icons.home_outlined),
-                label: 'Home',
-              ),
-              NavigationDestination(
-                icon: Icon(Icons.search_rounded, size: 29),
-                label: 'Search',
-              ),
-              NavigationDestination(
-                icon: Icon(Icons.settings_rounded),
-                label: 'Settings',
-              ),
-            ],
+            selectedIndex: currentPageIndex,
+            destinations: navDestinations,
           ),
         ));
   }
