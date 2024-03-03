1dc6b14c9e0596fc8eb5f3cb6989f9de32bee393
Rohan <rohgyas@hotmail.com>
2024-03-03 21:55:00 +0800

    Remove center wrapping & Parametrise size

--- lib/presentation/widgets/circular_progress_indicator.dart
+++ lib/presentation/widgets/circular_progress_indicator.dart
@@ -3,34 +3,33 @@ import 'package:loading_indicator/loading_indicator.dart';
 import 'package:tmdb/constants.dart';
 
 class CircularLoadingIndicator extends StatelessWidget {
-  const CircularLoadingIndicator({super.key});
+  const CircularLoadingIndicator({super.key, this.loadingIndicatorWidth = 20});
+
+  final double loadingIndicatorWidth;
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: [
-          const SizedBox(
-            width: 20,
-            child: LoadingIndicator(
-                indicatorType: Indicator.lineSpinFadeLoader,
-                colors: [MyColors.charcoal],
-                strokeWidth: 0,
-                backgroundColor: Colors.transparent,
-                pathBackgroundColor: Colors.black),
-          ),
-          const SizedBox(
-            height: 5,
-          ),
-          Text(
-            strings.loading,
-            style: Theme.of(context)
-                .textTheme
-                .displaySmall
-                ?.copyWith(fontSize: 12),
-          )
-        ],
-      ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(
+          width: loadingIndicatorWidth,
+          child: const LoadingIndicator(
+              indicatorType: Indicator.lineSpinFadeLoader,
+              colors: [MyColors.charcoal],
+              strokeWidth: 0,
+              backgroundColor: Colors.transparent,
+              pathBackgroundColor: Colors.black),
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        Text(
+          strings.loading,
+          style:
+              Theme.of(context).textTheme.displaySmall?.copyWith(fontSize: 12),
+        )
+      ],
     );
   }
 }
