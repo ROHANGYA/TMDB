f0f2dd32ecfd49c09cd6d7e5cdda54a5cd0a4cac
Rohan <rohgyas@hotmail.com>
2024-03-03 20:33:10 +0800

    Add background image to generic error widget & adjust spacings

--- lib/presentation/widgets/generic_error.dart
+++ lib/presentation/widgets/generic_error.dart
@@ -11,42 +11,55 @@ class GenericError extends StatelessWidget {
     return Card(
       color: Colors.white,
       margin: const EdgeInsets.symmetric(horizontal: 20),
-      child: Column(
-        children: [
-          const SizedBox(
-            height: 10,
+      child: Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+          image: AssetImage(
+            Assets.movieFilm,
           ),
-          const Icon(
-            Icons.warning_rounded,
-            color: MyColors.darkBlue,
-            size: 40,
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          Text(strings.errorHasOccurred,
-              style: Theme.of(context)
-                  .textTheme
-                  .displaySmall
-                  ?.copyWith(color: Colors.black, fontWeight: FontWeight.bold)),
-          const SizedBox(
-            height: 10,
-          ),
-          SizedBox(
-            width: 300,
-            child: Text(
-              errorDescription,
-              textAlign: TextAlign.center,
-              style: Theme.of(context)
-                  .textTheme
-                  .displaySmall
-                  ?.copyWith(color: Colors.black),
+          fit: BoxFit.contain,
+          repeat: ImageRepeat.repeatX,
+          opacity: 0.2,
+        )),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 20,
             ),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-        ],
+            const Icon(
+              Icons.warning_rounded,
+              color: MyColors.darkBlue,
+              size: 40,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(strings.errorHasOccurred,
+                style: Theme.of(context).textTheme.displaySmall?.copyWith(
+                    color: Colors.black, fontWeight: FontWeight.bold)),
+            const SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: SizedBox(
+                width: 270,
+                child: Text(
+                  errorDescription,
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context)
+                      .textTheme
+                      .displaySmall
+                      ?.copyWith(color: Colors.black),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+          ],
+        ),
       ),
     );
   }
