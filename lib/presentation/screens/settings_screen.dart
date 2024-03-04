6dd1e17efe05beb0eadfbe56181e114d289569dd
Rohan <rohgyas@hotmail.com>
2024-03-04 23:38:11 +0800

    Add starter settings page UI

--- lib/presentation/screens/settings_screen.dart
+++ lib/presentation/screens/settings_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tmdb/constants.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
@@ -11,11 +12,57 @@ class _SettingsScreenState extends State<SettingsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Settings"),
-      ),
-      body: const Center(
-        child: Text("Settings"),
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return [
+            SliverAppBar(
+              expandedHeight: 120,
+              pinned: true,
+              forceElevated: false,
+              flexibleSpace: FlexibleSpaceBar(
+                background: const ColoredBox(color: MyColors.darkBlue),
+                centerTitle: true,
+                titlePadding: const EdgeInsets.only(bottom: 10),
+                title: Text(
+                  strings.settings,
+                  style: Theme.of(context).textTheme.displayMedium,
+                ),
+              ),
+            )
+          ];
+        },
+        body: ListView(
+          children: [
+            Container(
+              height: 70,
+              margin: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(14),
+                  color: MyColors.vermillionRed),
+            ),
+            Container(
+              height: 70,
+              margin: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(14),
+                  color: MyColors.vermillionRed),
+            ),
+            Container(
+              height: 70,
+              margin: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(14),
+                  color: MyColors.vermillionRed),
+            ),
+            Container(
+              height: 70,
+              margin: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(14),
+                  color: MyColors.vermillionRed),
+            )
+          ],
+        ),
       ),
     );
   }
